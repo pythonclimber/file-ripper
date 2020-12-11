79800637e384f9927f4d7af7cbe91940f31af243
Aaron Smith <asmitty92@gmail.com>
2020-12-11 13:19:46 -0600

    Fix it

--- src/main/kotlin/FileRipper.kt
+++ src/main/kotlin/FileRipper.kt
@@ -25,11 +25,26 @@ class FileRipper(private val fileRepository: FileRepository, private val fileMov
         this.serviceFactory = serviceFactory
     }
 
+    /**
+     * Rip a file and return results as a Map<String, String>
+     * @param file File object to be ripped
+     * @param fileDefinition the file's configuration
+     * @return The data associated with the file.
+     */
     fun ripFile(file: File, fileDefinition: FileDefinition): FileOutput {
         val fileService = serviceFactory(fileDefinition)
         return fileService.processFile(file)
     }
 
+    /**
+     * Rip a file and return results as a List<T>
+     * @param T the type you want your file rows returned as
+     * @param file File object to be ripped
+     * @param fileDefinition the file's configuration
+     * @param recordBuilder function of type (Map<String, String> -> T) to build return individual
+     * response records
+     * @return The data associated with the file.
+     */
     fun <T> ripFile(file: File, fileDefinition: FileDefinition,
                     recordBuilder: (Map<String, String>) -> T): FileResult<T> {
         val fileOutput = ripFile(file, fileDefinition)
@@ -43,6 +58,12 @@ class FileRipper(private val fileRepository: FileRepository, private val fileMov
         return fileResult
     }
 
+    /**
+     * Rip multiple file and return results as a Map<String, String>
+     * @param files list of File objects to be ripped
+     * @param fileDefinition the file's configuration
+     * @return The data associated with the file.
+     */
     fun ripFiles(files: List<File>, fileDefinition: FileDefinition): List<FileOutput> {
         val outputList = files.map { f -> ripFile(f, fileDefinition) }
 
@@ -53,6 +74,15 @@ class FileRipper(private val fileRepository: FileRepository, private val fileMov
         return outputList
     }
 
+    /**
+     * Rip multiple files and return results as a List<T>
+     * @param T the type you want your file rows returned as
+     * @param files list of File objects to be ripped
+     * @param fileDefinition the file's configuration
+     * @param recordBuilder function of type (Map<String, String> -> T) to build return individual
+     * response records
+     * @return The data associated with the file.
+     */
     fun <T> ripFiles(files: List<File>, fileDefinition: FileDefinition,
                      recordBuilder: (Map<String, String>) -> T): List<FileResult<T>> {
         val resultList = files.map { f -> ripFile(f, fileDefinition, recordBuilder) }
@@ -64,11 +94,25 @@ class FileRipper(private val fileRepository: FileRepository, private val fileMov
         return resultList
     }
 
+    /**
+     * Find files, rip them, and return results as a Map<String, String>
+     * @param files list of File objects to be ripped
+     * @param fileDefinition the file's configuration
+     * @return The data associated with the file.
+     */
     fun findAndRipFiles(fileDefinition: FileDefinition): List<FileOutput> {
         val files = fileRepository.getFiles(fileDefinition.inputDirectory, fileDefinition.fileMask)
         return ripFiles(files, fileDefinition)
     }
 
+    /**
+     * Find files, rip them, and return results as a List<T>
+     * @param T the type you want your file rows returned as
+     * @param fileDefinition the file's configuration with must include an inputDirectory
+     * @param recordBuilder function of type (Map<String, String> -> T) to build return individual
+     * response records
+     * @return The data associated with the file.
+     */
     fun <T> findAndRipFiles(fileDefinition: FileDefinition, recordBuilder: (Map<String, String>) -> T):
             List<FileResult<T>> {
         val files = fileRepository.getFiles(fileDefinition.inputDirectory, fileDefinition.fileMask)
